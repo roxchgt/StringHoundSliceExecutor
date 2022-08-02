0694579c4704f9e44cb08c73d49d66a4b2614693
roxch <roshakz@gmail.com>
2022-08-02 09:56:34 +0200

    add Utils

---
+++ src/main/scala/slice/Utils.scala
@@ -0,0 +1,21 @@
+package slice
+
+object Utils {
+  implicit class ScopeFunctions[T](val obj: T) extends AnyVal {
+    /**
+     * Calls the specified function `block` with `this` value as its argument and returns `this`.
+     */
+    @inline
+    def also(block: T => Unit): T = {
+      block(obj)
+      obj
+    }
+
+    /**
+     * Calls the specified function `block` with `this` value as its argument and returns the result.
+     */
+    @inline
+    def let[A](block: T => A): A = block(obj)
+  }
+
+}
