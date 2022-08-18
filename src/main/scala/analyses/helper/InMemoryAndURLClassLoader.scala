102285288308ce4b863d1a72e1a53fa7f51e682e
roxch <roshakz@gmail.com>
2022-08-18 14:29:43 +0200

    add InMemoryAndURLClassLoader.scala

---
+++ src/main/scala/analyses/helper/InMemoryAndURLClassLoader.scala
@@ -0,0 +1,31 @@
+package analyses.helper
+
+import java.net.{URL, URLClassLoader}
+
+class InMemoryAndURLClassLoader(
+                                   private[this] var rawClasses: Map[String, Array[Byte]],
+                                   parent:                       ClassLoader              = ClassLoader.getSystemClassLoader,
+                                   urls:                         Array[URL]               = Array.empty
+                                 ) extends URLClassLoader(urls, parent) {
+
+    /**
+     * @note Clients should call `loadClass`! Please, consult the documentation of
+     *       `java.lang.ClassLoader` for further details!
+     */
+    @throws[ClassNotFoundException]
+    override def findClass(name: String): Class[_] = {
+      rawClasses.get(name) match {
+        case Some(data) ⇒
+          val clazz = defineClass(name, data, 0, data.length)
+          rawClasses -= name
+          clazz
+        case None ⇒
+          val loaded = findLoadedClass(name)
+          if (loaded == null) {
+            super.findClass(name)
+          } else {
+            loaded
+          }
+      }
+    }
+}
