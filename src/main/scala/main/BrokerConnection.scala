b1c0f4510ba2a8c8ebd6f31f05b2198d19c4fa76
roxch <roshakz@gmail.com>
2022-09-05 10:22:45 +0200

    remove ttl

--- src/main/scala/main/BrokerConnection.scala
+++ src/main/scala/main/BrokerConnection.scala
@@ -2,8 +2,6 @@ package main
 
 import com.rabbitmq.client.{Channel, Connection, ConnectionFactory}
 
-import java.util
-
 object BrokerConnection {
 
   private val config = Config
@@ -18,11 +16,7 @@ object BrokerConnection {
   val channel: Channel = connection.createChannel()
   channel.basicQos(1)
 
-  val arguments: java.util.Map[String, Object] = util.Map.of[String,Object](
-    "x-message-ttl", java.lang.Integer.valueOf(90000) // Message Time-To-Live on Queues
-  )
-
-  channel.queueDeclare(results_queue, false, false, false, arguments)
-  channel.queueDeclare(slices_queue, false, false, false, arguments)
+  channel.queueDeclare(results_queue, false, false, false, null)
+  channel.queueDeclare(slices_queue, false, false, false, null)
 
 }
