50a526237c6d1af59f2d3fd2d56778e0e2cc238c
roxch <roshakz@gmail.com>
2022-09-05 10:14:17 +0200

    refactor main function to use new execute

--- src/main/scala/main/StringHoundSliceExecutor.scala
+++ src/main/scala/main/StringHoundSliceExecutor.scala
@@ -1,29 +1,31 @@
 package main
 
-import analyses.slicing.SliceExtract
+import analyses.slicing.{SliceExtract, SliceExtractExecutor}
 import com.rabbitmq.client._
 import main.BrokerConnection.{channel, results_queue, slices_queue}
 import org.apache.commons.lang3.SerializationUtils
 import org.slf4j.{Logger, LoggerFactory}
-
-import scala.util.Try
-
 
 object StringHoundSliceExecutor extends App {
 
   val logger: Logger = LoggerFactory.getLogger(StringHoundSliceExecutor.getClass)
   val brokerConnection = BrokerConnection
 
-
   val deliverCallback: DeliverCallback = (tag: String, delivery: Delivery) => {
-    logger.info(s"– received slice of jar: {}", delivery.getProperties.getAppId)
     val deliveryTag: Long = delivery.getEnvelope.getDeliveryTag
+    val jarName = delivery.getProperties.getAppId
     try {
-      val sliceExtract: SliceExtract = SerializationUtils.deserialize[SliceExtract](delivery.getBody)
-      val result: Try[List[String]] = sliceExtract.execute()
+      val sliceExtract: SliceExtract =
+        SerializationUtils.deserialize(delivery.getBody)
+
+      val result =
+        sliceExtract.executeWith(sliceExtractExecutor = new SliceExtractExecutor(jarName))
+
       val serializedResult = SerializationUtils.serialize(result)
+
       channel.basicPublish("", results_queue, delivery.getProperties, serializedResult)
       channel.basicAck(deliveryTag, false)
+
     } catch {
       case x: Exception =>
         logger.error(x.getMessage)
