7de1ff7454fb5c72f462310dd40f49838a91a143
roxch <roshakz@gmail.com>
2022-09-06 10:36:52 +0200

    fix arrangement

--- src/main/scala/main/Config.scala
+++ src/main/scala/main/Config.scala
@@ -3,10 +3,11 @@ package main
 import com.typesafe.config._
 
 object Config {
+  private val conf = ConfigFactory.load()
+
   val brokerHost: String      = conf.getString("broker-host")
   val isAndroid: Boolean      = conf.getBoolean("is-android")
   val stopExecutionAfter: Int = conf.getInt("stop-execution-after")
   val logSlicing: Boolean     = conf.getBoolean("log-slicing")
   val debug: Boolean          = conf.getBoolean("debug")
-  private val conf            = ConfigFactory.load()
 }
