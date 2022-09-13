aba1b43cf1c8f9edbebfb9e723efa3a69aee6004
roxch <roshakz@gmail.com>
2022-09-13 17:47:54 +0200

    refactor consumer

--- src/main/scala/main/StringHoundSliceExecutor.scala
+++ src/main/scala/main/StringHoundSliceExecutor.scala
@@ -8,39 +8,43 @@ import org.slf4j.{Logger, LoggerFactory}
 
 object StringHoundSliceExecutor extends App {
 
-  val logger: Logger                                     = LoggerFactory.getLogger(StringHoundSliceExecutor.getClass)
-  val brokerConnection                                   = BrokerConnection
+  val logger: Logger = LoggerFactory.getLogger(StringHoundSliceExecutor.getClass)
+  val brokerConnection = BrokerConnection
   var sliceExtractExecutor: Option[SliceExtractExecutor] = None
 
   private def getExecutor(jarName: String): SliceExtractExecutor = {
-    if (sliceExtractExecutor.isEmpty || sliceExtractExecutor.get.getJarName != jarName) {
+    if (!sliceExtractExecutor.exists(executor => executor.getJarName.equals(jarName))) {
       sliceExtractExecutor = Option[SliceExtractExecutor](new SliceExtractExecutor(jarName))
     }
     sliceExtractExecutor.get
   }
 
-  val deliverCallback: DeliverCallback = (tag: String, delivery: Delivery) => {
-    val deliveryTag: Long = delivery.getEnvelope.getDeliveryTag
-    val jarName           = delivery.getProperties.getAppId
-    try {
-      val sliceExtract: SliceExtract =
-        SerializationUtils.deserialize(delivery.getBody)
-
-      val result =
-        sliceExtract.executeWith(getExecutor(jarName))
-
-      val serializedResult = SerializationUtils.serialize(result)
-
-      channel.basicPublish("", results_queue, delivery.getProperties, serializedResult)
-      channel.basicAck(deliveryTag, false)
-
-    } catch {
-      case x: Exception =>
-        logger.error(x.getMessage)
-        channel.basicReject(deliveryTag, false)
-    }
-  }
-
   logger.info("waiting for incoming slice extracts ...")
-  channel.basicConsume(slices_queue, false, deliverCallback, (tag: String) => {})
+  channel.basicConsume(
+    slices_queue,
+    false,
+    (tag: String, delivery: Delivery) => {
+      val deliveryTag: Long = delivery.getEnvelope.getDeliveryTag
+
+      val jarName = delivery.getProperties.getAppId
+      try {
+        val sliceExtract: SliceExtract = SerializationUtils.deserialize(delivery.getBody)
+
+        val result = sliceExtract.executeWith(getExecutor(jarName))
+
+        val serializedResult = SerializationUtils.serialize(result)
+
+        channel.basicPublish("", results_queue, delivery.getProperties, serializedResult)
+        channel.basicAck(deliveryTag, false)
+
+      } catch {
+        case x: Exception =>
+          logger.error(x.getMessage)
+          channel.basicReject(deliveryTag, false)
+      }
+    },
+    (tag: String) => {
+      logger.info(s"Consumer {} cancelled.", tag)
+    }
+  )
 }
