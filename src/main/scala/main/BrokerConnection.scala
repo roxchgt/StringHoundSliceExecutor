71e8ead704b33713626eb5c17a80feb3f5cef0de
roxch <roshakz@gmail.com>
2022-09-14 13:35:53 +0200

    results queue with TTL 30 sec

--- src/main/scala/main/BrokerConnection.scala
+++ src/main/scala/main/BrokerConnection.scala
@@ -2,6 +2,8 @@ package main
 
 import com.rabbitmq.client.{Channel, Connection, ConnectionFactory}
 
+import java.util
+
 object BrokerConnection {
 
   private val config = Config
@@ -11,12 +13,15 @@ object BrokerConnection {
 
   val factory: ConnectionFactory = new ConnectionFactory
   factory.setHost(config.brokerHost)
-  val connection: Connection = factory.newConnection()
 
-  val channel: Channel = connection.createChannel()
+  val connection: Connection = factory.newConnection
+  val channel: Channel       = connection.createChannel
+  val args                   = new util.HashMap[String, Object]
+  args.put("x-message-ttl", Integer.valueOf(30000))
+
   channel.basicQos(1)
 
-  channel.queueDeclare(results_queue, false, false, false, null)
+  channel.queueDeclare(results_queue, false, false, false, args)
   channel.queueDeclare(slices_queue, false, false, false, null)
 
 }
